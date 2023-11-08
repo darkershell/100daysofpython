e23dbad30ad66c3dbf83266a5b6511f7e2bc784c
Richard Nixon <rnixonaf@gmail.com>
2023-11-08 18:16:21 +0000

    project2-tip calculator

--- project2.py
+++ project2.py
@@ -1,10 +1,38 @@
 
-#BMI calculation
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? $\n"))
+tip = float(input("what percentage tip would you like to give? 10, 12, or  15? \n"))
+people = int (input("How many people to split the bill? \n"))
+result  = (tip / bill) * 100.0
+result_final = result / people
+print(f"Each person should pay: {result_final}")
 
-weight = int(input ("your weight"))
-height = int(input ("your height"))
-bmi = weight / height ** 2
-print (bmi)
+
+
+
+# #Life in Weeks.56
+# # if we live until 90y old
+
+# age = int(input("how old are you?"))
+# process_age = ((90 - age) * 52)
+# print(f"You have {age} and you have more {process_age} weeks, enjoy!")
+
+
+
+# score = 0
+# height = 1.8
+# isWinning = True
+# #f-String
+# print(f"your score is {score},  your height is {height}, you are winning is {isWinning}")
+
+
+
+# #BMI calculation
+
+# weight = float(input ("your weight: "))
+# height = float(input ("your height: "))
+# bmi =  int(weight / height ** 2)
+# print (bmi)
 
 
 ## precedence order calculation
