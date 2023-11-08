13c8bdcc254ecce8e05f5816d2e6b967a8a3a33c
Richard Nixon <rnixonaf@gmail.com>
2023-11-08 08:01:00 +0000

    ex2

---
+++ project2.py
@@ -0,0 +1,23 @@
+
+#BMI calculation
+
+weight = int(input ("your weight"))
+height = int(input ("your height"))
+bmi = weight / height ** 2
+print (bmi)
+
+
+## precedence order calculation
+# print(3 * 3 + 3 / 3 - 3) # 7
+# print(3 * (3 + 3) / 3 - 3) #3
+
+
+
+
+
+## datas type exercise
+
+# number = input("Type the number:")
+# number1 = int(number[0])
+# number2 = int(number[1])
+# print(number1+number2)80
