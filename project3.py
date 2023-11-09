8c0865e8aac835aa3dd22e5cd6990d37c72deed6
Richard Nixon <rnixonaf@gmail.com>
2023-11-09 17:55:32 +0000

    project3 started

---
+++ project3.py
@@ -0,0 +1,16 @@
+num1 = int(input("type a number: "))
+
+if num1 % 2 == 0:
+    print("It's Even!")
+else:
+    print("It's Odd!")
+
+
+
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller vefore you can ride")
